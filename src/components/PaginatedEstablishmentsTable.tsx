138b7de2ece0264bc3510273e10626766ad96bac
Radovan Kuka <kuka.radovan@gmail.com>
2022-09-07 17:30:04 +0200

    UC-2: show loading text when fetching establishments

--- src/components/PaginatedEstablishmentsTable.tsx
+++ src/components/PaginatedEstablishmentsTable.tsx
@@ -3,57 +3,59 @@ import { EstablishmentsTable } from "./EstablishmentsTable";
 import { EstablishmentsTableNavigation } from "./EstablishmentsTableNavigation";
 import { getEstablishmentRatings } from "../api/ratingsAPI";
 
-const tableStyle = {
+const tableStyle: { [key: string]: string | number } = {
   background: "rgba(51, 51, 51, 0.9)",
   padding: "10px",
   width: "max-content",
   marginLeft: "50px",
   color: "white",
+  minHeight: "400px",
+  minWidth: "550px",
+  display: "flex",
+  flexDirection: "column"
+};
+
+const loaderStyle: { [key: string]: string | number } = {
+  flex: 1,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
 };
 
 export const PaginatedEstablishmentsTable = () => {
-  const [error, setError] =
-    useState<{ message: string; [key: string]: string }>();
-  const [establishments, setEstablishments] = useState<
-    { [key: string]: string }[]
-  >([]);
-  const [pageNum, setPageNum] = useState(1);
+  const [error, setError] = useState<{ message: string; [key: string]: string }>();
+  const [establishments, setEstablishments] = useState<{ [key: string]: string }[]>([]);
+  const [pageNum, setPageNum] = useState<number>(1);
+  const [isLoading, setLoading] = useState<boolean>(false);
   const [pageCount] = useState(100);
 
   useEffect(() => {
-    getEstablishmentRatings(pageNum).then(
-      (result) => {
-        setEstablishments(result?.establishments);
-      },
-      (error) => {
-        setError(error);
-      }
-    );
+    fetchEstablishmentRatingsPage(pageNum);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  async function handlePreviousPage() {
+  function handlePreviousPage() {
     pageNum > 1 && setPageNum(pageNum - 1);
-    getEstablishmentRatings(pageNum).then(
-      (result) => {
-        setEstablishments(result.establishments);
-      },
-      (error) => {
-        setError(error);
-      }
-    );
+    fetchEstablishmentRatingsPage(pageNum);
   }
 
-  async function handleNextPage() {
+  function handleNextPage() {
     pageNum < pageCount && setPageNum(pageNum + 1);
-    getEstablishmentRatings(pageNum).then(
-      (result) => {
-        setEstablishments(result.establishments);
-      },
-      (error) => {
-        setError(error);
-      }
-    );
+    fetchEstablishmentRatingsPage(pageNum);
+  }
+
+  async function fetchEstablishmentRatingsPage(page: number): Promise<void> {
+    try {
+      setLoading(true);
+      setEstablishments([]);
+
+      const result = await getEstablishmentRatings(pageNum);
+      setEstablishments(result.establishments);
+    } catch(error: any) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   if (error) {
@@ -62,7 +64,14 @@ export const PaginatedEstablishmentsTable = () => {
     return (
       <div style={tableStyle}>
         <h2>Food Hygiene Ratings</h2>
-        <EstablishmentsTable establishments={establishments} />
+        <EstablishmentsTable
+          establishments={establishments}
+        />
+
+        {
+          isLoading && <span style={loaderStyle}>Loading...</span>
+        }
+
         <EstablishmentsTableNavigation
           pageNum={pageNum}
           pageCount={pageCount}
