051b1c414901f7a77ae383d9bef58d01f4cf8c37
Polycarp Okock <deaspo@users.noreply.github.com>
2024-01-25 11:36:17 +0100

    - Setup Tailwind in the CRA, react-router-dom - fix failing App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,11 @@
-import { render } from "@testing-library/react";
-import App from "./App";
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
 
-test("renders learn react link", () => {
-  render(<App />);
+test('renders learn react link', () => {
+	render(
+		<MemoryRouter>
+			<App />
+		</MemoryRouter>,
+	);
 });
